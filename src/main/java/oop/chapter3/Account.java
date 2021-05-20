49bfac35ce4538354ec3eebb21c135aa88f20d66
meroa006 <merouani.ass@gmail.com>
2021-05-20 11:04:14 -0400

    Initial commit

---
+++ src/main/java/oop/chapter3/Account.java
@@ -0,0 +1,14 @@
+package oop.chapter3;
+
+public class Account {
+    private String name; // class instance variable; maintains data for each object.
+
+    // set name
+    public void setName(String name){
+        this.name= name;
+    }
+    // get name
+    public String getName(){
+        return name;
+    }
+}
---
+++ src/main/java/oop/chapter3/AccountTest.java
@@ -0,0 +1,7 @@
+package oop.chapter3;
+
+public class AccountTest {
+    public static void main(String[] args){
+        System.out.printf("test main !");
+    }
+}
