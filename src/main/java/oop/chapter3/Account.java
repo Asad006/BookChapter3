4eb2ca599f576ad29ea8cce7543b9d11f3cf0f01
meroa006 <merouani.ass@gmail.com>
2021-05-20 21:34:25 -0400

    test success

--- src/main/java/oop/chapter3/Account.java
+++ src/main/java/oop/chapter3/Account.java
@@ -4,6 +4,8 @@ public class Account {
     private String name; // class instance variable; maintains data for each object.
 
     // set name
+    //// (string name, type param, ...) are local variables as they are located
+    // in the parameter list of the method.can be used only in the method.
     public void setName(String name){
         this.name= name;
     }
--- src/main/java/oop/chapter3/AccountTest.java
+++ src/main/java/oop/chapter3/AccountTest.java
@@ -1,7 +1,25 @@
 package oop.chapter3;
 
+import java.util.Scanner;
+// class containing the method main is called the driver class.
 public class AccountTest {
     public static void main(String[] args){
-        System.out.printf("test main !");
+        System.out.printf("test main !\n");
+        // creation a scanner object;
+        Scanner sc = new Scanner(System.in);
+
+        // create an object Account
+        Account account1= new Account();
+
+        // display the name of the account which gives as the default value
+        // of the instance variable: the default value of strings is 'NULL'
+        System.out.printf("account name %s%n", account1.getName());
+
+        // se the name
+        System.out.println("Enter the account name\n");
+        account1.setName(sc.nextLine());
+
+        // display the name of the account
+        System.out.printf("account name is %s%n", account1.getName());
     }
 }
